d9e4bcae146d429829d6ed6f913de19b5b1f3fe5
Maichol <msibiriu@gmail.com>
2020-11-30 15:14:49 +0100

    navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 // javascript
+const app = new Vue({
+  el: "#wplms",
+  data: {
+
+    // img logo header
+    img: "logo",
+
+    // array oggetti icone e link header
+    navbarList:{
+      link:[
+        "home",
+        "courses",
+        "pages",
+        "app",
+      ],
+      iconNavbar: "fa-search",
+    },
+    account:{
+      link: "login",
+      icon:"fa-shopping-cart",
+    },
+
+
+
+  }
+
+
+
+});
