f30fff2a134a4d792cd3d63ee05253b851cf3105
Maichol <msibiriu@gmail.com>
2020-12-01 15:54:30 +0100

    inizio 3 sezione di main

--- js/main.js
+++ js/main.js
@@ -22,7 +22,36 @@ const app = new Vue({
     },
 
     // raccolta foto header
-    collection: "fa-images"
+    collection: "fa-images",
+
+    // main
+    // array oggetti sezione free app
+    settingsApp:[
+      {
+        icon: 'fa-address-card',
+        title: 'Administrator App',
+        text: 'Manage the entire site,the students, the istructor and the store.',
+      },
+      {
+        icon: 'fa-film',
+        title: 'Instructor App',
+        text: 'Create courses, manage students, import questions and get reports',
+      },
+      {
+        icon: 'fa-tv',
+        title: 'Student App',
+        text: 'Easy access to enrolled courses. Pursue courses, join meetings in TV',
+      },
+      {
+        icon: 'fa-server',
+        title: 'Custom Features',
+        text: 'All Wplms modules in the App. Conferencing, Calendars.',
+      },
+
+    ],
+
+    // icon btn
+    iconBtn: "fa-angle-right",
 
   }
 
